e55e5b3cffe0e4e5c3ab53c6d511dbaaa2c66b16
Ebrahim Essa Ahmed <99309335+12288987@users.noreply.github.com>
2025-01-28 10:23:58 +0300

    finished the script

---
+++ script.js
@@ -0,0 +1,91 @@
+let startBtn = document.getElementById('start');
+let stopBtn = document.getElementById('stop');
+let resetBtn = document.getElementById('reset');
+
+let hour = 0;
+let minute = 0;
+let second = 0;
+let count = 0;
+
+startBtn.addEventListener('click', function () {
+    timer = true;
+    stopWatch();
+});
+
+stopBtn.addEventListener('click', function () {
+    timer = false;
+    let lapnum = 0;
+    if (document.getElementById('laptime').innerHTML==="") {
+        document.getElementById('laptime').innerHTML = "First lap time: " + document.getElementById('hr').innerHTML + "hr " + document.getElementById('min').innerHTML + "min " + document.getElementById('sec').innerHTML + "sec " + document.getElementById('count').innerHTML;
+        lapnum++;
+    } else {
+        document.getElementById('laptime').innerHTML = document.getElementById('laptime').innerHTML + "</br>" + "Latest lap time: " + document.getElementById('hr').innerHTML + "hr " + document.getElementById('min').innerHTML + "min " + document.getElementById('sec').innerHTML + "sec " + document.getElementById('count').innerHTML;
+    }
+    // document.getElementById('txthr').innerHTML = 'hr';
+    // document.getElementById('minl').innerHTML = document.getElementById('min').innerHTML;
+    // document.getElementById('txtmin').innerHTML = 'min';
+    // document.getElementById('secl').innerHTML = document.getElementById('sec').innerHTML;
+    // document.getElementById('txtsec').innerHTML = 'sec';
+    // document.getElementById('countl').innerHTML = document.getElementById('count').innerHTML;
+});
+
+resetBtn.addEventListener('click', function () {
+    timer = false;
+    hour = 0;
+    minute = 0;
+    second = 0;
+    count = 0;
+    document.getElementById('hr').innerHTML = "00";
+    document.getElementById('min').innerHTML = "00";
+    document.getElementById('sec').innerHTML = "00";
+    document.getElementById('count').innerHTML = "00";
+});
+
+function stopWatch() {
+    if (timer) {
+        count++;
+
+        if (count == 100) {
+            second++;
+            count = 0;
+        }
+
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+            second = 0;
+        }
+
+        let hrString = hour;
+        let minString = minute;
+        let secString = second;
+        let countString = count;
+
+        if (hour < 10) {
+            hrString = "0" + hrString;
+        }
+
+        if (minute < 10) {
+            minString = "0" + minString;
+        }
+
+        if (second < 10) {
+            secString = "0" + secString;
+        }
+
+        if (count < 10) {
+            countString = "0" + countString;
+        }
+
+        document.getElementById('hr').innerHTML = hrString;
+        document.getElementById('min').innerHTML = minString;
+        document.getElementById('sec').innerHTML = secString;
+        document.getElementById('count').innerHTML = countString;
+        setTimeout(stopWatch, 10);
+    }
+}
